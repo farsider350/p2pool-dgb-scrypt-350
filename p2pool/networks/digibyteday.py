1796dda2dd58b12d393815e2338c352071699852
farsider350 <31876349+farsider350@users.noreply.github.com>
2018-09-01 08:43:53 +1000

    2 Servers

--- p2pool/networks/digibyteday.py
+++ p2pool/networks/digibyteday.py
@@ -1,20 +1,20 @@
 from p2pool.bitcoin import networks
 
-PARENT=networks.nets['digibytebru']
-SHARE_PERIOD = 25
-CHAIN_LENGTH = 24*60*60//10
-REAL_CHAIN_LENGTH = 24*60*60//10
-TARGET_LOOKBEHIND = 200
+PARENT=networks.nets['digibyteday']
+SHARE_PERIOD = 15
+CHAIN_LENGTH = 1*24*60
+REAL_CHAIN_LENGTH = 1*24*60
+TARGET_LOOKBEHIND = 60
 SPREAD = 30
-IDENTIFIER = '48242A6E7BF4DFEB'.decode('hex')
-PREFIX = '48242A6E405A15EB'.decode('hex')
+IDENTIFIER = '31878C3D13481C8D'.decode('hex')
+PREFIX = '2358B404B4781CD0'.decode('hex')
 P2P_PORT = 5024
 MIN_TARGET = 0
 MAX_TARGET = 2**256//2**20 - 1
-PERSIST = True
+PERSIST = False
 WORKER_PORT = 5055
-BOOTSTRAP_ADDRS = 'dgbsolo.brutang.work pocketburgers.ddnsfree.com 204.93.127.197 triplezen.tk'.split(' ')
-ANNOUNCE_CHANNEL = '#p2pool'
+BOOTSTRAP_ADDRS = 'triplezen.tk'.split(' ')
+ANNOUNCE_CHANNEL = '#triplezen'
 VERSION_CHECK = lambda v: None if 6160400 <= v else 'DigiByte version too old. Upgrade to 6.16.4 or newer!'
 VERSION_WARNING = lambda v: None
 SOFTFORKS_REQUIRED = set(['nversionbips', 'csv', 'segwit'])
