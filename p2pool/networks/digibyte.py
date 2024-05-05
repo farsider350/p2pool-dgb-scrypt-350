06070ccbbfac1368163bce67b1b95d39138d8598
farsider350 <31876349+farsider350@users.noreply.github.com>
2024-05-05 19:08:25 +1000

    Add new bootstraps to join network

--- p2pool/networks/digibyte.py
+++ p2pool/networks/digibyte.py
@@ -11,14 +11,15 @@ PREFIX = '1bfe01eff652e4b7'.decode('hex')
 P2P_PORT = 5024
 MIN_TARGET = 0
 MAX_TARGET = 2**256//2**20 - 1
-PERSIST = False
+PERSIST = True
 WORKER_PORT = 5025
 BOOTSTRAP_ADDRS = [
-        'triplezen.org',
-        'zen-solo.com',
+        'dgb.triplezen.org',
+        'dgb-east.triplezen.org',
+        'dgb-west.triplezen.org',
         ]
 ANNOUNCE_CHANNEL = '#p2pool'
-VERSION_CHECK = lambda v: None if 82200 <= v else 'DigiByte version too old. Upgrade to 7.17.2 or newer!'
+VERSION_CHECK = lambda v: None if 82200 <= v else 'DigiByte version too old. Upgrade to 8.2.2 or newer!'
 VERSION_WARNING = lambda v: None
 SOFTFORKS_REQUIRED = set(['csv', 'segwit', 'odo', 'testdummy'])
 MINIMUM_PROTOCOL_VERSION = 1600
